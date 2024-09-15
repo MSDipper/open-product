dd0a68f39bca84ebf65e72d772f4196b895429c1
msd <github5248@gmail.com>
2024-09-15 20:12:37 +0300

    update

--- src/entities/Product/Product.tsx
+++ src/entities/Product/Product.tsx
@@ -1,16 +1,21 @@
 import { ProductProps } from "./Product.props";
 import styles from './Product.module.scss';
-import { Image, Title } from "../../shared/ui";
+import { Button, Image, Title } from "../../shared/ui";
 
 export const Product = ({ }: ProductProps) => {
     return (
         <div className={styles.component}>
             <div>
                 <Image src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdk5ijcsq4PFXlfss1vXUvsl_Liu0utqmrS-M7ZNM7YalkbgEpLjRFF3J2_9A7X_gfUNM&usqp=CAU' alt="" />
-                <Title >Title</Title>
-                <p className={styles.text}>
-                    Это затычка в виде описания под этим постом в виде затычки вроде пока пойдет посмотрим потом на что можно будет поменять на текст рыбу вполне.
-                </p>
+                <div className={styles.content}>
+                    <p>33.00&#8381;</p>
+                    <p className={styles.text}>
+                        Это затычка в виде описания под этим постом в виде затычки вроде пока пойдет посмотрим потом на что можно будет поменять на текст рыбу вполне.
+                    </p>
+                    <div>
+                        <Button>Добавить в корзину</Button>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -4,3 +4,4 @@ export { Input } from './Input';
 export { Navbar } from './Navbar';
 export { Panel } from './Panel';
 export { Title } from './Title';
+export { Button } from './Button';
